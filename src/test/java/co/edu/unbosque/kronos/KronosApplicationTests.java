a456e3553972696dcac8758a9e8227b061a104ef
Alphathesniper21 <sebastiantoyo91@gmail.com>
2026-09-10 17:52:24 -0500

    Estructura inicial de MVC con el framework de Springboot

---
+++ src/test/java/co/edu/unbosque/kronos/KronosApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.kronos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KronosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
